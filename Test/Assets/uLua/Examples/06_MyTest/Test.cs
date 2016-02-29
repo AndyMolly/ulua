dcb06bf48809e90e9f35faecd1b5e80f76a8456e
Andy <1187820970@qq.com>
2016-02-29 20:25:01 +0800

    ulua 测试例子

---
+++ Test/Assets/uLua/Examples/06_MyTest/Test.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using LuaInterface;
+
+public class Test : MonoBehaviour {
+
+	public TextAsset testScript;
+	private const string script=@"
+            function mytest(message)
+              print(message)
+              return 100
+            end
+          ";
+	// Use this for initialization
+	void Start () {
+	
+		LuaState l = new LuaState ();
+		l.DoString (script);
+		var func = l.GetFunction ("mytest");
+	    var r=	func.Call ("this is my test for ulua");
+		print (r [0]);
+
+		print ("通过脚本调用");
+
+		l.DoString (testScript.text);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
